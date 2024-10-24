82d15839b8d35647cacb0541262b6e6fcdc7dc27
reagan luyinda <luyindareagan@gmail.com>
2024-10-24 16:11:05 +0300

    ref; changed text and bg color of the Nav bar
    changed the text color to zinc-500 and background color to zinc-50

--- snap-webpage/src/components/navbar.tsx
+++ snap-webpage/src/components/navbar.tsx
@@ -1,22 +1,22 @@
 import { Link } from "react-router-dom";
 function Navbar() {
   return (
-    <div className="flex px-16 border-2 items-center py-4 bg-slate-100">
+    <div className="flex px-16 border-2 items-center py-4  bg-zinc-50 ">
       <div className="flex-initial w-32  text-3xl font-bold">
         <Link to="/">Snap</Link>
       </div>
-      <div className="flex-1 space-x-8 font-semibold">
+      <div className="flex-1 space-x-8 font-semibold text-zinc-500">
         <Link to="/Features">Features</Link>
         <Link to="/">Company</Link>
         <Link to="/">Careers</Link>
         <Link to="/">About</Link>
       </div>
-      <div className="space-x-8">
+      <div className="space-x-8 text-zinc-500">
         <Link to="/Login" className=" font-semibold">
           Login
         </Link>
         <Link to="/Register">
-          <button className=" border-2 border-black px-4 py-2 rounded-xl">
+          <button className=" border-2 border-black px-4 py-2 rounded-xl font-semibold text-zinc-500">
             Register
           </button>
         </Link>
