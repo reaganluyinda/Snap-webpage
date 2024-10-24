065cc55317c2707477482469ad2e0471e08b197a
reagan luyinda <luyindareagan@gmail.com>
2024-10-25 00:54:30 +0300

    ref; added media queries for the small / medium and large screen

--- snap-webpage/src/pages/home.tsx
+++ snap-webpage/src/pages/home.tsx
@@ -7,32 +7,30 @@ import maker from "../assets/client-maker.svg";
 
 function Home() {
   return (
-    <div className="bg-zinc-50 py-16">
-      <div className="container mx-auto grid md:grid-cols-2 md:items-center md:gap-16 lg:px-48">
-        <div className="md:order-2 ">
+    <div className="bg-white py-16">
+      <div className="container mx-auto">
+        <div>
           <img src={smallImage} alt="Small Image" className="block sm:hidden" />
           <img
             src={largeImage}
             alt="Large Image"
-            className="hidden sm:block  h-70 w-96"
+            className="hidden sm:block"
           ></img>
         </div>
-        <div className=" text-center md:text-left py-8 ">
-          <h1 className="text-4xl md:text-6xl font-bold leading-20">
-            Make Remote work
-          </h1>
-          <div className="text-base md:text-lg font-medium py-4 md:py-12 leading-7 md:pr-24 text-zinc-500">
+        <div className=" text-center md:text-left py-8">
+          <h1 className="text-4xl font-bold">Make Remote work</h1>
+          <div className="text-base font-medium py-4">
             <p>
               Get your team in sync, no matter your location. Streamline
               processes, create team rituals, and watch productivity soar.
             </p>
           </div>
           <div>
-            <button className="bg-black font-semibold text-white px-4 py-2  rounded-xl border-2 text-center">
+            <button className="bg-black font-semibold text-white px-4 py-2 rounded-xl border-2 text-center">
               Learn more
             </button>
           </div>
-          <div className=" flex gap-4 pt-10 justify-center md:justify-start md:pt-24">
+          <div className=" flex gap-4 pt-10 justify-center">
             <img src={databiz} alt="databiz" />
             <img src={audiophile} alt="audiophile" />
             <img src={meet} alt="meet" />
