24ad84d422e1f102375a537d96052382154e8984
reagan luyinda <luyindareagan@gmail.com>
2024-10-24 12:58:08 +0300

    ref; added styles to the Navbar

--- snap-webpage/src/components/navbar.tsx
+++ snap-webpage/src/components/navbar.tsx
@@ -1,16 +1,26 @@
 import { Link } from "react-router-dom";
 function Navbar() {
   return (
-    <div>
-      <Link to="/">Snap</Link>
-      <Link to="/Features">Features</Link>
-      <Link to="/">Company</Link>
-      <Link to="/">Careers</Link>
-      <Link to="/">About</Link>
-      <Link to="/Login">Login</Link>
-      <Link to="/Register">
-        <button>Register</button>
-      </Link>
+    <div className="flex px-16 border-2 items-center py-4 bg-slate-100">
+      <div className="flex-initial w-32  text-3xl font-bold">
+        <Link to="/">Snap</Link>
+      </div>
+      <div className="flex-1 space-x-8 font-semibold">
+        <Link to="/Features">Features</Link>
+        <Link to="/">Company</Link>
+        <Link to="/">Careers</Link>
+        <Link to="/">About</Link>
+      </div>
+      <div className="space-x-8">
+        <Link to="/Login" className=" font-semibold">
+          Login
+        </Link>
+        <Link to="/Register">
+          <button className=" border-2 border-black px-4 py-2 rounded-xl">
+            Register
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
